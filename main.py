c3bc67e804e8ab3be68b704e33133a2e55762c04
Kryshtoson <krystof.chytry@gmail.com>
2023-03-06 14:26:46 +0100

    new script

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import os
 import pandas as pd
 
-file=open("floristak_input",'r')
+file = open("floristak_input",'r',encoding="utf8")
 
-c=file.readlines()
+c = file.readlines()
+
 for outer_i in range(0, len(c)):
     one_line = c[outer_i]
     one_sp = ''
@@ -16,34 +18,49 @@ for outer_i in range(0, len(c)):
     print(one_sp)
 
     data = []
+    remarks = []
     one = ''
+    remark = ''
     remark_mode = False
+
     for i in range(len(one_sp), len(one_line)):
-        #print(str(i) + ': ' + str(one_line[i]))
+        print(str(i) + ': ' + str(one_line[i]))
         if one_line[i] == '(':
             remark_mode = True
+            #one_line[i] = ''
 
         if one_line[i] == ')':
             remark_mode = False
+            #one_line[i] = ''
 
         if remark_mode == True:
-            one = one + one_line[i]
+            if one_line[i] != '(':
+                remark = remark + one_line[i]
 
         elif one_line[i] != ',':
             if one_line[i] != ' ':
                 if one_line[i] != ':':
-                    one = one + one_line[i]
+                    if one_line[i] != ')':
+                        if one_line[i] != '\n':
+                            one = one + one_line[i]
 
         elif one_line[i] == ',':
             data.append(one)
+            remarks.append(remark)
             one = ''
+            remark = ''
 
-        elif one_line[i] == '\n':
+        if one_line[i] == '\n':
             data.append(one)
+            remarks.append(remark)
             one = ''
+            remark = ''
 
     if outer_i == 0:
-        out_data = pd.DataFrame({'species': [one_sp] * len(data), 'data': data})
+        out_data = pd.DataFrame({'species': [one_sp] * len(data), 'data': data, 'remarks': remarks})
 
     else:
-        out_data = pd.concat(out_data, pd.DataFrame({'species': [one_sp] * len(data), 'data': data}))
+        out_data = pd.concat([out_data, pd.DataFrame({'species': [one_sp] * len(data), 'data': data, 'remarks': remarks})])
+
+out_data['data'] = [int(i) for i in out_data['data']]
+out_data.to_excel('floristak_out.xlsx', index=False)
